d6357cce68e466539724ef8dd7dc3a91d627195d
Dimas <dimas.maulanads@gmail.com>
2019-10-10 06:57:07 +0700

    Initial Commit

--- js/controller/sw-controller.js
+++ js/controller/sw-controller.js
@@ -1,12 +1,1 @@
-const swSetup = () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(swReg => {
-            console.log('Service Worker Registered ', swReg);
-            swRegistration = swReg;
-        });
-    } else {
-        console.log('Failed to register service worker')
-    }
-};
-
-swSetup();
+// TODO: Register Service Worker here!
